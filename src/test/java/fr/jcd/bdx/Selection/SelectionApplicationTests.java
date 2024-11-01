8eda0804b2aec46357ae4520125d1c15c3bdb65a
jchahid <jalal.chahid.86@gmail.com>
2024-11-01 22:57:03 +0100

    second project version

---
+++ src/test/java/fr/jcd/bdx/Selection/SelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jcd.bdx.Selection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
